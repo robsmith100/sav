1f62ff40b4118d46ba0191c0945f3450570dcd9b
Rob Smith <rob@metricstudios.com>
2021-12-17 11:22:21 -0700

    converted project to build using tsc

--- src/test.js
+++ src/test.js
@@ -7,7 +7,7 @@ const cc = {
     BgBlack: "\x1b[40m", BgRed: "\x1b[41m", BgGreen: "\x1b[42m", BgYellow: "\x1b[43m", BgBlue: "\x1b[44m", BgMagenta: "\x1b[45m", BgCyan: "\x1b[46m", BgWhite:"\x1b[47m",
 }
 
-const filename = "test-data/generic dataset 6.sav";
+const filename = "../test-data/generic dataset 6.sav";
 
 
 async function test1(){
